d590528899e385ddd3af11dc6d63731127bf93fe
GaneshChintalapaty <ganesh.chintalapaty@gmail.com>
2025-04-15 10:28:19 -0500

    TCPServer UI Complete

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/TCPServer_autogen/include/ui_tcpserver.h
+++ build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/TCPServer_autogen/include/ui_tcpserver.h
@@ -19,6 +19,7 @@
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
@@ -28,6 +29,14 @@ class Ui_TCPServer
 {
 public:
     QWidget *centralwidget;
+    QWidget *horizontalLayoutWidget_3;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QPushButton *pushButton_2;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout_3;
+    QTextBrowser *textBrowser;
     QWidget *widget;
     QVBoxLayout *verticalLayout_2;
     QHBoxLayout *horizontalLayout_2;
@@ -37,7 +46,6 @@ public:
     QLabel *labelPort;
     QSpacerItem *horizontalSpacer_2;
     QLineEdit *TCP_Port;
-    QSpacerItem *horizontalSpacer;
     QPushButton *pushButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
@@ -46,9 +54,41 @@ public:
     {
         if (TCPServer->objectName().isEmpty())
             TCPServer->setObjectName("TCPServer");
-        TCPServer->resize(592, 406);
+        TCPServer->resize(592, 400);
         centralwidget = new QWidget(TCPServer);
         centralwidget->setObjectName("centralwidget");
+        horizontalLayoutWidget_3 = new QWidget(centralwidget);
+        horizontalLayoutWidget_3->setObjectName("horizontalLayoutWidget_3");
+        horizontalLayoutWidget_3->setGeometry(QRect(10, 330, 571, 31));
+        horizontalLayout_4 = new QHBoxLayout(horizontalLayoutWidget_3);
+        horizontalLayout_4->setObjectName("horizontalLayout_4");
+        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
+        label = new QLabel(horizontalLayoutWidget_3);
+        label->setObjectName("label");
+
+        horizontalLayout_4->addWidget(label);
+
+        lineEdit = new QLineEdit(horizontalLayoutWidget_3);
+        lineEdit->setObjectName("lineEdit");
+
+        horizontalLayout_4->addWidget(lineEdit);
+
+        pushButton_2 = new QPushButton(horizontalLayoutWidget_3);
+        pushButton_2->setObjectName("pushButton_2");
+
+        horizontalLayout_4->addWidget(pushButton_2);
+
+        verticalLayoutWidget = new QWidget(centralwidget);
+        verticalLayoutWidget->setObjectName("verticalLayoutWidget");
+        verticalLayoutWidget->setGeometry(QRect(10, 90, 571, 231));
+        verticalLayout_3 = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout_3->setObjectName("verticalLayout_3");
+        verticalLayout_3->setContentsMargins(0, 0, 0, 0);
+        textBrowser = new QTextBrowser(verticalLayoutWidget);
+        textBrowser->setObjectName("textBrowser");
+
+        verticalLayout_3->addWidget(textBrowser);
+
         widget = new QWidget(centralwidget);
         widget->setObjectName("widget");
         widget->setGeometry(QRect(10, 10, 571, 81));
@@ -78,18 +118,15 @@ public:
 
         horizontalLayout_3->addWidget(labelPort);
 
-        horizontalSpacer_2 = new QSpacerItem(22, 20, QSizePolicy::Policy::Fixed, QSizePolicy::Policy::Minimum);
+        horizontalSpacer_2 = new QSpacerItem(35, 20, QSizePolicy::Policy::Fixed, QSizePolicy::Policy::Minimum);
 
         horizontalLayout_3->addItem(horizontalSpacer_2);
 
         TCP_Port = new QLineEdit(widget);
         TCP_Port->setObjectName("TCP_Port");
+        TCP_Port->setAlignment(Qt::AlignmentFlag::AlignCenter);
 
         horizontalLayout_3->addWidget(TCP_Port);
-
-        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Minimum);
-
-        horizontalLayout_3->addItem(horizontalSpacer);
 
         pushButton = new QPushButton(widget);
         pushButton->setObjectName("pushButton");
@@ -116,10 +153,13 @@ public:
     void retranslateUi(QMainWindow *TCPServer)
     {
         TCPServer->setWindowTitle(QCoreApplication::translate("TCPServer", "TCPServer", nullptr));
+        label->setText(QCoreApplication::translate("TCPServer", "Message: ", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("TCPServer", "Send", nullptr));
         labelIPAddress->setText(QCoreApplication::translate("TCPServer", "IP Address: ", nullptr));
         TCP_IP_Address->setText(QCoreApplication::translate("TCPServer", "192.168.30.100", nullptr));
         labelPort->setText(QCoreApplication::translate("TCPServer", "Port: ", nullptr));
-        pushButton->setText(QCoreApplication::translate("TCPServer", "PushButton", nullptr));
+        TCP_Port->setText(QCoreApplication::translate("TCPServer", "6031", nullptr));
+        pushButton->setText(QCoreApplication::translate("TCPServer", "Connect", nullptr));
     } // retranslateUi
 
 };
